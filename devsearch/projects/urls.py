ba7371e3bf37fb133759825894d7a99eba09b508
MD. MANZURUL ISLAM <khanabid7474@gmail.com>
2023-02-20 03:11:03 +0600

    update ,delete,add

--- devsearch/projects/urls.py
+++ devsearch/projects/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.projects,name='projects'),
     path('project/<str:pk>/', views.single_project,name='project'),
     path('create/', views.create_project,name='createproject'),
+    path('update/<str:pk>', views.update_project,name='updateproject'),
+    path('delete/<str:pk>', views.delete_project,name='delete'),
 ]
--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -19,6 +19,7 @@ def single_project(request, pk):
 
 
 def create_project(request):
+    form = ProjectForm()
     if request.method == 'POST':
         form = ProjectForm(request.POST)
         if form.is_valid():
@@ -27,3 +28,23 @@ def create_project(request):
     context = {'form':form}
     return render(request, 'projects/project_form.html',context)
 
+
+def update_project(request, pk):
+    prt = project.objects.get(id=pk)
+    form = ProjectForm(instance = prt)
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=prt)
+        if form.is_valid():
+            form.save()
+            return redirect('projects')
+    context = {'form':form}
+    return render(request, 'projects/project_form.html',context)
+
+
+def delete_project(request, pk):
+    ptr = project.objects.get(id=pk)
+    if request.method == 'POST':
+        ptr.delete()
+        return redirect('projects')
+    context = {'object':ptr}
+    return render(request, 'projects/delete.html',context)
